0f1e1df720cf4f82886eea99b826813cf5305c62
Ayush Rastogi <ayushrastogi689@gmail.com>
2023-07-14 19:42:15 +0530

    Functionality - Changing the background color of main section and showing on DOM

--- Random Color Generator/app.js
+++ Random Color Generator/app.js
@@ -1,11 +1,14 @@
 function getColor() {
   // Generating Random number using Math.random();
   const randomNumber = Math.floor(Math.random() * 16777215);
-  
+
   // passed 16 to convert it into hexa decimal number
   const hexColorCode = "#" + randomNumber.toString(16);
 
   //console.log(randomNumber, hexColorCode);
+
+  // Changing the background color of body
+  document.body.style.backgroundColor = hexColorCode;
 }
 
 // 2. Event Calling and Listening
