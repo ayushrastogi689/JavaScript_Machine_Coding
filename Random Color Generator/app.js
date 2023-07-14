eb74e9cbbad8db1b12ac610dfecefefd1fc547d1
Ayush Rastogi <ayushrastogi689@gmail.com>
2023-07-14 19:39:34 +0530

    Added a hexColorCode generator

--- Random Color Generator/app.js
+++ Random Color Generator/app.js
@@ -1,4 +1,9 @@
 function getColor() {
-    // Generating Random number using Math.random();
-    const randomNumber = Math.random();
+  // Generating Random number using Math.random();
+  const randomNumber = Math.floor(Math.random() * 16777215);
+  
+  // passed 16 to convert it into hexa decimal number
+  const hexColorCode = "#" + randomNumber.toString(16);
+
+  //console.log(randomNumber, hexColorCode);
 }
