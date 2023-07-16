fe5b602fc462a8c6d55ccfdcfd0733f2614eb2b1
Ayush Rastogi <ayushrastogi689@gmail.com>
2023-07-16 17:42:29 +0530

    Generating random password

--- Password Generator/app.js
+++ Password Generator/app.js
@@ -19,15 +19,32 @@ const characters = {
 
 function generatePassword() {
     let staticPassword = "";
+    let randomPassword = "";
+    let excludeDuplicates = false;
+    let passwordLength = lengthSlider.value;
     // Looping through each option check box
     options.forEach(option => {
         // if any option is checked add that particular key value from characters object to that staticPassword
         if (option.checked)
         {
-            staticPassword = staticPassword + characters[option.id];
+            if (option.id !== "duplicates" && option.id !== "spaces") {
+                staticPassword = staticPassword + characters[option.id];
+            }
+            else if(option.id !== "spaces"){
+                staticPassword = staticPassword + `${staticPassword}`
+            }
+            else {
+                excludeDuplicates = true;
+            }
         }
     })
-    console.log(staticPassword);
+    
+    for (let i = 0; i < passwordLength; i++)
+    {
+        randomPassword = randomPassword + staticPassword[Math.floor(Math.random() * staticPassword.length)]
+        
+    }
+    
 }
 
 lengthSlider.addEventListener("input", updateSliderValue);
