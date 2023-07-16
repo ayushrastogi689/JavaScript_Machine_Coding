59a58e91c256aa3402ec889457c48b41e0137147
Ayush Rastogi <ayushrastogi689@gmail.com>
2023-07-16 17:19:51 +0530

    Password Generation Functionality - Added a function to create password

--- Password Generator/app.js
+++ Password Generator/app.js
@@ -1,9 +1,25 @@
 const lengthSlider = document.querySelector(".password-length input");
 
+
+const password = document.querySelector(".generate-btn"); 
+
 function updateSliderValue() {
     document.querySelector(".password-length span").innerText = lengthSlider.value;
 }
 updateSliderValue();
 
+function generatePassword() {
+    let staticPassword = "";
+    // Looping through each option check box
+    options.forEach(option => {
+        // if any option is checked add that particular key value from characters object to that staticPassword
+        if (option.checked)
+        {
+            staticPassword = staticPassword + characters[option.id];
+        }
+    })
+    console.log(staticPassword);
+}
 
 lengthSlider.addEventListener("input", updateSliderValue);
+password.addEventListener("click", generatePassword)
