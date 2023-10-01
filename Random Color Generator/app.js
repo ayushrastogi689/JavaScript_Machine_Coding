1bb52daebccaeb194485b3a89d91747ef8b5477d
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-10-01 20:37:06 +0530

    Update app.js

--- Random Color Generator/app.js
+++ Random Color Generator/app.js
@@ -1,36 +1,3 @@
-const changeColorButton = document.querySelector("#button");
-
-function getRandomColor() {
-  const letters = "0123456789abcdef";
-
-  let hex = "#", randomCode = "";
-
-  for (let i = 0; i < 6; i++) {
-    randomCode = randomCode + letters[Math.round(Math.random() * 16)];
-  }
-
-  let hexColorCode = hex + randomCode;
-
-  // showing the color code on DOM
-  document.getElementById("color-code").innerText = hexColorCode;
-
-  // Copy code to clipboard
-  navigator.clipboard.writeText(hexColorCode);
-
-  return hexColorCode;
-}
-
-function changeBackgroundColor() {
-  document.body.style.backgroundColor = getRandomColor();
-}
-
-changeColorButton.addEventListener("click", changeBackgroundColor);
-
-changeBackgroundColor();
-
-
-// Another way of implementing random color generation
-
 function getColor() {
   // Generating Random number using Math.random();
   const randomNumber = Math.floor(Math.random() * 16777215);
@@ -55,4 +22,4 @@ document.getElementById("btn").addEventListener("click", getColor);
 
 // 1. Initial Call
 // Calling getColor() function for First time load 
-// getColor();
+getColor();
